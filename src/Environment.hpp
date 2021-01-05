0915b349704adfd55b65363df4719e7cfac03502
bgailleton <boris.gailleton@gfz-potsdam.de>
2021-01-05 18:50:24 +0100

    Still in the same task, getting there :camel:

--- src/Environment.hpp
+++ src/Environment.hpp
@@ -493,11 +493,13 @@ class ModelRunner
   std::priority_queue< node_to_reproc, std::vector<node_to_reproc>, std::greater<node_to_reproc> >& ORDEEEEEER,
    std::vector<char>& is_in_queue, int outlet);
     void preprocess_outletting_chonk(chonk& tchonk, EntryPoint& entry_point, int current_lake, int outlet,
- std::map<int,double>& WF_corrector, std::map<int,double>& SF_corrector, std::map<int,std::vector<double> >& SL_corrector);
+ std::map<int,double>& WF_corrector, std::map<int,double>& SF_corrector, std::map<int,std::vector<double> >& SL_corrector,
+ std::vector<double>& pre_sed, std::vector<double>& pre_water, std::vector<int>& pre_entry_node, std::vector<std::vector<double> >& label_prop_of_pre);
     void check_what_give_to_existing_outlets(std::map<int,double>& WF_corrector,  std::map<int,double>& SF_corrector, 
   std::map<int,std::vector<double> >&  SL_corrector, std::vector<int>& local_mstack);
     bool is_this_node_in_this_lake(int node, int tlake){bool out = false; int lakid = this->node_in_lake[node]; if(lakid>=0)lakid = this->motherlake(lakid);if(lakid==tlake)out = true;return out;}
-
+    void check_what_give_to_existing_lakes(std::vector<int>& local_mstack, int outlet, int current_lake, std::vector<double>& this_sed,
+   std::vector<double>& this_water, std::vector<int>& this_entry_node, std::vector<std::vector<double> >& label_prop_of_this);
 
 
     std::vector<int> lake_in_order;
